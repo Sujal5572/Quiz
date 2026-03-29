2b17af8647bfc55f684d2f679c147a428bd419ac
Sujal Arya <sujalarya17@gmail.com>
2026-03-29 17:31:50 +0530

    added few checks

--- quiz-service/src/main/java/com/telusko/quizservice/controller/QuizController.java
+++ quiz-service/src/main/java/com/telusko/quizservice/controller/QuizController.java
@@ -22,15 +22,17 @@ public class QuizController {
         return quizService.createQuiz(quizDto.getCategoryName(), quizDto.getNumQuestions(), quizDto.getTitle());
     }
 
+    @PostMapping("submit/{id}")
+    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses){
+        return quizService.calculateResult(id, responses);
+    }
+
     @PostMapping("get/{id}")
     public ResponseEntity<List<QuestionWrapper>> getQuizQuestions(@PathVariable Integer id){
         return quizService.getQuizQuestions(id);
     }
 
-    @PostMapping("submit/{id}")
-    public ResponseEntity<Integer> submitQuiz(@PathVariable Integer id, @RequestBody List<Response> responses){
-        return quizService.calculateResult(id, responses);
-    }
+
 
 
 }
